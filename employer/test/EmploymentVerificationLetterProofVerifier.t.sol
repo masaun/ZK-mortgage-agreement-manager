af8e09d29bb28d2d645e85b7b8a98abcccabab71
masaun <masaun@protonmail.com>
2025-03-09 21:23:43 +0900

    [Employer - SC test]: Implement the test_verifyProof() /w the .withStruct() and the .withStructInput()

--- employer/test/EmploymentVerificationLetterProofVerifier.t.sol
+++ employer/test/EmploymentVerificationLetterProofVerifier.t.sol
@@ -36,21 +36,13 @@ contract EmploymentVerificationLetterProofVerifierTest is Test {
                   .withInput("annual_salary", bytes32(uint256(55000)))
                   .withStruct("employeeVerificationLetterInfo")
                   .withStructInput("employee_name", bytes32(uint256(1)))
-                  //.withStruct("employeeVerificationLetterInfo")
                   .withStructInput("employee_address", bytes32(uint256(1)))
-                  //.withStruct("employeeVerificationLetterInfo")
                   .withStructInput("employee_phone_number", bytes32(uint256(1)))
-                  //.withStruct("employeeVerificationLetterInfo")
                   .withStructInput("title", bytes32(uint256(1)))
-                  //.withStruct("employeeVerificationLetterInfo")
                   .withStructInput("contract_type", bytes32(uint256(1)))
-                  //.withStruct("employeeVerificationLetterInfo")
                   .withStructInput("annual_salary_in_struct", bytes32(uint256(55000)))
-                  //.withStruct("employeeVerificationLetterInfo")
                   .withStructInput("employment_start_date", bytes32(uint256(1741127219)))
-                  //.withStruct("employeeVerificationLetterInfo")
                   .withStructInput("employment_end_date", bytes32(uint256(1772663219)))
-                  //.withStruct("employeeVerificationLetterInfo")
                   .withStructInput("letter_created_date", bytes32(uint256(1741073225)));
                   //.withProjectPath("./circuits/circuit-for-employer"); /// @dev - Custom file path for the circuit file.
 
@@ -59,11 +51,44 @@ contract EmploymentVerificationLetterProofVerifierTest is Test {
     }
 
     function test_wrongProof() public {
-        // noirHelper.clean();
-        // noirHelper.withInput("x", 1).withInput("y", 5).withInput("return", 5);
-        // (bytes32[] memory publicInputs, bytes memory proof) = noirHelper.generateProof("test_wrongProof", 2);
-        // vm.expectRevert();
-        // employmentVerificationLetterProofVerifier.verifyEqual(proof, publicInputs);
+        noirHelper.clean();
+        
+        uint256[] memory hash_path = new uint256[](2);
+        hash_path[0] = 0x1efa9d6bb4dfdf86063cc77efdec90eb9262079230f1898049efad264835b6c8;
+        hash_path[1] = 0x2a653551d87767c545a2a11b29f0581a392b4e177a87c8e3eb425c51a26a8c77;
+
+        bytes32[] memory hash_path_bytes32 = new bytes32[](2);
+        hash_path_bytes32[0] = bytes32(hash_path[0]);
+        hash_path_bytes32[1] = bytes32(hash_path[1]);
+
+        noirHelper.withInput("root", bytes32(uint256(0x215597bacd9c7e977dfc170f320074155de974be494579d2586e5b268fa3b629)))
+                  .withInput("hash_path", hash_path_bytes32)
+                  .withInput("index", bytes32(uint256(0)))
+                  .withInput("secret", bytes32(uint256(1))) /// @dev - [NOTE]: 'Field' type in Noir must be the form of this (= bytes32(uint256(XXX))).
+                  .withInput("annual_salary", bytes32(uint256(55000)))
+                  .withStruct("employeeVerificationLetterInfo")
+                  .withStructInput("employee_name", bytes32(uint256(1)))
+                  .withStructInput("employee_address", bytes32(uint256(1)))
+                  .withStructInput("employee_phone_number", bytes32(uint256(1)))
+                  .withStructInput("title", bytes32(uint256(1)))
+                  .withStructInput("contract_type", bytes32(uint256(1)))
+                  .withStructInput("annual_salary_in_struct", bytes32(uint256(55000)))
+                  .withStructInput("employment_start_date", bytes32(uint256(1741127219)))
+                  .withStructInput("employment_end_date", bytes32(uint256(1772663219)))
+                  .withStructInput("letter_created_date", bytes32(uint256(1741073225)));
+                  //.withProjectPath("./circuits/circuit-for-employer"); /// @dev - Custom file path for the circuit file.
+
+        (bytes32[] memory publicInputs, bytes memory proof) = noirHelper.generateProof("test_wrongProof", 3);
+
+        /// @dev - This should fail because the public input is wrong
+        uint256 fake_annual_salary = 60000;
+        bytes32[] memory fakePublicInputs = new bytes32[](3);
+        fakePublicInputs[0] = publicInputs[0];
+        fakePublicInputs[1] = bytes32(uint256(fake_annual_salary));  // @dev - This is wrong publicInput ("_annual_salary") - when this proof was geneerated.
+        fakePublicInputs[2] = publicInputs[2];
+
+        vm.expectRevert();
+        employmentVerificationLetterProofVerifier.verifyEqual(proof, fakePublicInputs);
     }
 
     // function test_all() public {
