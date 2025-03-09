7d1b000e88710dde17f507bafcb005c57b4c2fb7
masaun <masaun@protonmail.com>
2025-03-09 12:00:30 +0900

    [Employer - SC test]: Assign the property data of the 'EmployeeVerificationLetterInfo' struct into the noirHelper.withInput() /w the .withStruct() and .withStructInput()

--- test/employer/EmploymentVerificationLetterProofVerifier.t.sol
+++ test/employer/EmploymentVerificationLetterProofVerifier.t.sol
@@ -9,7 +9,7 @@ import "forge-std/Test.sol";
 import {NoirHelper} from "foundry-noir-helper/NoirHelper.sol";
 
 
-contract EmploymentVerificationLetterProofVerifierOnSonicTestnetTest is Test {
+contract EmploymentVerificationLetterProofVerifierTest is Test {
     EmploymentVerificationLetterProofVerifier public employmentVerificationLetterProofVerifier;
     UltraVerifier public verifier;
     NoirHelper public noirHelper;
@@ -21,8 +21,37 @@ contract EmploymentVerificationLetterProofVerifierOnSonicTestnetTest is Test {
     }
 
     function test_verifyProof() public {
-        noirHelper.withInput("x", 1).withInput("y", 1).withInput("return", 1);
-        (bytes32[] memory publicInputs, bytes memory proof) = noirHelper.generateProof("test_verifyProof", 2);
+        uint256[] memory hash_path = new uint256[](2);
+        hash_path[0] = 0x1efa9d6bb4dfdf86063cc77efdec90eb9262079230f1898049efad264835b6c8;
+        hash_path[1] = 0x2a653551d87767c545a2a11b29f0581a392b4e177a87c8e3eb425c51a26a8c77;
+
+        bytes32[] memory hash_path_bytes32 = new bytes32[](2);
+        hash_path_bytes32[0] = bytes32(hash_path[0]);
+        hash_path_bytes32[1] = bytes32(hash_path[1]);
+
+        noirHelper.withInput("root", bytes32(uint256(0x215597bacd9c7e977dfc170f320074155de974be494579d2586e5b268fa3b629)))
+                  .withInput("hash_path", hash_path_bytes32)
+                  //.withInput("hash_path", hash_path)
+                  .withInput("index", bytes32(uint256(0)))
+                  .withInput("secret", bytes32(uint256(1))) /// @dev - [NOTE]: 'Field' type in Noir must be the form of this (= bytes32(uint256(XXX))).
+                  .withInput("annual_salary", bytes32(uint256(55000)))
+                  .withStruct("employeeVerificationLetterInfo")
+                  .withStructInput("employee_name", bytes32(uint256(1)))
+                  .withStruct("employeeVerificationLetterInfo")
+                  .withStructInput("employee_address", bytes32(uint256(1)))
+                  .withStruct("employeeVerificationLetterInfo")
+                  .withStructInput("employee_phone_number", bytes32(uint256(1)))
+                  .withStruct("employeeVerificationLetterInfo")
+                  .withStructInput("title", bytes32(uint256(1)))
+                  .withStruct("employeeVerificationLetterInfo")
+                  .withStructInput("contract_type", bytes32(uint256(1)))
+                  .withStruct("employeeVerificationLetterInfo")
+                  .withStructInput("annual_salary_in_struct", bytes32(uint256(55000)))
+                  .withStructInput("employment_start_date", bytes32(uint256(1741127219)))
+                  .withStructInput("employment_end_date", bytes32(uint256(1772663219)))
+                  .withStructInput("letter_created_date", bytes32(uint256(1741073225)));
+
+        (bytes32[] memory publicInputs, bytes memory proof) = noirHelper.generateProof("test_verifyProof", 3);
         employmentVerificationLetterProofVerifier.verifyEqual(proof, publicInputs);
     }
 
