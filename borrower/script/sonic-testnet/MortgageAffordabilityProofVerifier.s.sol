e2a771949103312676288de0a98e433f3e4407c3
masaun <masaun@protonmail.com>
2025-03-10 09:21:12 +0900

    [Borrower - All]: Make the 'borrower' directory an independent directory

---
+++ borrower/script/sonic-testnet/MortgageAffordabilityProofVerifier.s.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+import { UltraVerifier } from "../../../contracts/borrower/circuit/plonk_vk.sol";
+//import "../../../circuits/circuit-for-borrower/target/contract.sol";
+import "../../../contracts/borrower/MortgageAffordabilityProofVerifier.sol";
+
+contract MortgageAffordabilityProofVerifierScript is Script {
+    MortgageAffordabilityProofVerifier public mortgageAffordabilityProofVerifier;
+    UltraVerifier public verifier;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.createSelectFork("sonic_blaze_testnet");
+        uint256 deployerPrivateKey = vm.envUint("SONIC_BLAZE_TESTNET_PRIVATE_KEY");
+        //uint256 deployerPrivateKey = vm.envUint("LOCALHOST_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        address ULTRA_VERIFIER = vm.envAddress("ULTRAVERIFER_CONTRACT_ADDRESS_ON_SONIC_TESTNET");
+        address STARTER = vm.envAddress("MORTGAGE_AFFORDABILITY_PROOF_VERIFIER_CONTRACT_ADDRESS_ON_SONIC_TESTNET");
+        verifier = UltraVerifier(ULTRA_VERIFIER);
+        //verifier = new UltraVerifier();
+        mortgageAffordabilityProofVerifier = MortgageAffordabilityProofVerifier(STARTER);
+        //mortgageAffordabilityProofVerifier = new MortgageAffordabilityProofVerifier(verifier);
+    }
+}
---
+++ borrower/test/runningTest_MortgageAffordabilityProofVerifier.sh
@@ -0,0 +1,2 @@
+echo "Running the test of the MortgageAffordabilityProofVerifierTest..."
+forge test --optimize --optimizer-runs 5000 --evm-version cancun --match-contract MortgageAffordabilityProofVerifierTest -vvv
