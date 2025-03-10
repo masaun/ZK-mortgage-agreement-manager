34b624dadeba75b868efc76c8fac8f70399e0ccc
masaun <masaun@protonmail.com>
2025-03-10 22:05:48 +0900

    [Borrower - SC test]: Update the setUp() + Add the test_scenario() to the MortgageAgreementManagerOnSonicTestnet.t.sol

--- borrower/test/sonic-testnet/MortgageAgreementManagerOnSonicTestnet.t.sol
+++ borrower/test/sonic-testnet/MortgageAgreementManagerOnSonicTestnet.t.sol
@@ -4,7 +4,7 @@ import { IEmploymentVerificationLetterProofVerifier } from "../../../contracts/i
 import { IFICOCreditScoreProofVerifier } from "../../contracts/interfaces/FICO/IFICOCreditScoreProofVerifier.sol";
 //import { FICOCreditScoreProofVerifier } from "../../../FICO/contracts/FICOCreditScoreProofVerifier.sol";
 import { MortgageAffordabilityProofVerifier } from "../../contracts/MortgageAffordabilityProofVerifier.sol";
-//import { MortgageAffordabilityProofVerifier } from "../../contracts/MortgageAffordabilityProofVerifier.sol";
+import { MortgageAgreementManager } from "../../contracts/MortgageAgreementManager.sol";
 
 import { DataTypes } from "../../contracts/libraries/DataTypes.sol";
 
@@ -13,17 +13,19 @@ import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
 
-contract MortgageAffordabilityProofVerifierTest is Test {
+contract MortgageAgreementManagerOnSonicTestnetTest is Test {
     NoirHelper public noirHelper;
 
     IEmploymentVerificationLetterProofVerifier public employmentVerificationLetterProofVerifier;
     IFICOCreditScoreProofVerifier public ficoCreditScoreProofVerifier;
     MortgageAffordabilityProofVerifier public mortgageAffordabilityProofVerifier;
+    MortgageAgreementManager public mortgageAgreementManager;
 
     /// @dev - Read the each deployed address from the configuration file.
     address EMPLOYMENT_VERIFICATION_LETTER_PROOF_VERIFIER = vm.envAddress("EMPLOYMENT_VERIFICATION_LETTER_PROOF_VERIFIER_ON_SONIC_TESTNET");
     address FICO_CREDIT_SCORE_PROOF_VERIFIER = vm.envAddress("FICO_CREDIT_SCORE_PROOF_VERIFIER_ON_SONIC_TESTNET");
     address MORTGAGE_AFFORDABILITY_PROOF_VERIFIER = vm.envAddress("MORTGAGE_AFFORDABILITY_PROOF_VERIFIER_ON_SONIC_TESTNET");
+    address MORTGAGE_AGREEMENT_MANAGER = vm.envAddress("MORTGAGE_AGREEMENT_MANAGER_ON_SONIC_TESTNET");
 
     function setUp() public {
         noirHelper = new NoirHelper();
@@ -31,6 +33,12 @@ contract MortgageAffordabilityProofVerifierTest is Test {
         employmentVerificationLetterProofVerifier = IEmploymentVerificationLetterProofVerifier(EMPLOYMENT_VERIFICATION_LETTER_PROOF_VERIFIER);
         ficoCreditScoreProofVerifier = IFICOCreditScoreProofVerifier(FICO_CREDIT_SCORE_PROOF_VERIFIER);
         mortgageAffordabilityProofVerifier = MortgageAffordabilityProofVerifier(MORTGAGE_AFFORDABILITY_PROOF_VERIFIER);
+        mortgageAgreementManager = MortgageAgreementManager(MORTGAGE_AGREEMENT_MANAGER);
+    }
+
+    function test_scenario() public {
+        /// [TODO]:
+        console.log("test");
     }
 
 }
--- borrower/test/sonic-testnet/runningTest_MortgageAgreementManagerOnSonicTestnet.sh
+++ borrower/test/sonic-testnet/runningTest_MortgageAgreementManagerOnSonicTestnet.sh
@@ -5,5 +5,5 @@ source .env
 # Set the RPC URL for the Sonic Testnet
 #export SONIC_BLAZE_TESTNET_RPC="https://rpc.ankr.com/electroneum_testnet"
 
-echo "Running the test of the MortgageAgreementManagerTest on Sonic Testnet..."
-forge test --optimize --optimizer-runs 5000 --evm-version cancun --match-contract MortgageAgreementManagerTest -vv --rpc-url ${SONIC_BLAZE_TESTNET_RPC}
+echo "Running the test of the MortgageAgreementManager on Sonic Testnet..."
+forge test --optimize --optimizer-runs 5000 --evm-version cancun --match-contract MortgageAgreementManagerOnSonicTestnetTest -vv --rpc-url ${SONIC_BLAZE_TESTNET_RPC}
